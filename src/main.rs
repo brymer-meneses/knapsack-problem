fc155fe863e43751f9c3c847a78ec98ff4de7ec1
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2024-10-26 20:43:32 +0800

    improve code
    Make use of the algorithm presented in the book (Exercise 9b) on section
    4.3 (Algorithms Levitin)

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,121 @@
 use std::fmt;
+use rand::Rng;
 use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
 
-use rand::Rng;
-
-
-#[derive(Clone, Copy, Debug)]
-struct KnapsackItem {
-    weight: u8,
-    value: u16,
-}
-
+#[derive(Copy, Clone)]
 struct BitString {
-    data: u64,
-    length: u8,
+    data: u64
 }
 
-impl fmt::Display for KnapsackItem {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({} {})", self.value, self.weight)
+impl BitString {
+    fn new(data: u64) -> Self {
+        Self { data  }
+    }
+
+    fn is_bit_set(&self, index: usize) -> bool {
+        (self.data & (1u64 << index)) != 0
+    }
+
+    fn least_significant_bit(&self) -> usize {
+        self.data.trailing_zeros() as usize
+    }
+
+    fn flip_bit(&mut self, index: usize) {
+        self.data ^= 1 << index;
     }
 }
 
 impl fmt::Display for BitString {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:050b}", self.data)
+        write!(f, "{:08b}", self.data)
     }
 }
 
-impl BitString {
-    fn new(data: u64, length: u8) -> BitString {
-        return BitString {data, length}
+struct Knapsack {
+    weights: Vec<u16>,
+    values: Vec<u16>,
+    max_weight: u64,
+    total_items: usize,
+}
+
+impl Knapsack {
+    fn new(max_weight: u64, total_items: usize) -> Self {
+        Self {
+            weights: Vec::with_capacity(total_items as usize),
+            values: Vec::with_capacity(total_items as usize),
+            max_weight,
+            total_items,
+        }
     }
 
-    fn generate_subset<T: Copy>(&self, items: &[T]) -> Vec<T> {
-        let mut result = vec![];
-        for bit in 0..self.length {
-            if self.is_bit_set(bit) {
-                result.push(items[bit as usize])
+    fn set_value_and_weight(&mut self, value: u16, weight: u16) {
+        self.weights.push(weight);
+        self.values.push(value);
+    }
+
+
+    fn compute_value_and_weight(&self, subset: BitString) -> (u64, u64) {
+        return (0..self.total_items).fold((0, 0), |acc, index| {
+            if subset.is_bit_set(index) {
+                (acc.0 + self.values[index] as u64, acc.1 + self.weights[index] as u64)
+            } else {
+                acc
+            }
+        })
+    }
+
+    fn solve(&self) -> (BitString, u64) {
+
+        // we ignore the 0 bit string since it doesn't have any value
+        let mut bit_str = BitString::new(0);
+
+        let mut max_value = u64::MIN;
+        let mut best_subset = BitString::new(0);
+
+        let n = 1 << self.total_items;
+
+        let pb = ProgressBar::new(n as u64);
+            pb.set_style(
+                ProgressStyle::with_template(
+                    "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta})",
+                )
+                .unwrap(),
+            );
+
+        for i in (1..n).progress_with(pb) {
+            let lsb = BitString::new(i).least_significant_bit();
+            bit_str.flip_bit(lsb);
+
+            let (value, weight) = self.compute_value_and_weight(bit_str);
+
+            if weight > self.max_weight {
+                continue
+            }
+
+            if max_value < value {
+                max_value = value;
+                best_subset = bit_str;
             }
         }
-        return result;
-    }
 
-    fn is_bit_set(&self, index: u8) -> bool {
-        (self.data & (1u64 << index)) != 0
+        return (best_subset, max_value)
     }
 }
 
-fn solve_knapsack(items: &[KnapsackItem], max_weight: u64) -> (Vec<KnapsackItem>, u64) {
-    let length = items.len();
-    let n = (2 as usize).pow(length as u32);
-
-    let mut current_value = std::u64::MIN;
-    let mut current_subset = vec![];
-    let pb = ProgressBar::new(n as u64);
-    pb.set_style(
-        ProgressStyle::with_template(
-            "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta})",
-        )
-        .unwrap(),
-    );
-
-    for i in (0..n).progress_with(pb) {
-        let bit_string = BitString::new(i as u64, length as u8);
-        let subset = bit_string.generate_subset(items);
-        let (weight, value) = subset.iter().fold((0u64, 0u64), |(acc_weight, acc_value), item| {
-            (acc_weight + item.weight as u64,  acc_value + item.value as u64)
-        });
-        if weight > max_weight{
-            continue;
-        }
-
-        if value > current_value {
-            current_value = value;
-            current_subset = subset.clone();
-        }
-    }
-
-    return (current_subset, current_value)
-}
 
 
 fn main() {
-    let items: Vec<KnapsackItem> = (0..30).map(|_| {
-        KnapsackItem {
-            weight: rand::thread_rng().gen_range(50..=100), 
-            value: rand::thread_rng().gen_range(100..=500)
-        }
-    }).collect();
+    let size = 35;
+    let mut knapsack = Knapsack::new(1000, size);
 
-    let (subset, value)= solve_knapsack(&items, 1000);
+    for _ in 0..size {
+        let weight = rand::thread_rng().gen_range(50..=100);
+        let value = rand::thread_rng().gen_range(100..=500);
 
-    println!("Found {:?}", subset);
-    println!("Value {}", value);
+        knapsack.set_value_and_weight(value, weight);
+    }
+
+    let (subset, value) = knapsack.solve();
+
+    println!("{subset} {value}");
 }
-
-#[test]
-fn test_generate_subset() {
-    let bit_string = BitString::new(0b111, 3);
-    let subset = bit_string.generate_subset(&[1, 2, 3]);
-    assert_eq!(subset, [1, 2, 3])
-}
-
